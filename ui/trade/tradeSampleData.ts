1ff40dc9cc8a7e00110a344b38b0649233fd0e80
sraibaby <somewallet@gmail.com>
2026-05-29 23:56:43 +0300

    Add sample trade feed items and define TradeFeedItem type
    - Introduced a new type `TradeFeedItem` to structure trade feed data.
    - Added sample trade feed items to `TRADE_SAMPLE_FEED_ITEMS` for testing and development purposes.
    - Enhanced the existing trade sample data file to support new feed item structure.

--- ui/trade/tradeSampleData.ts
+++ ui/trade/tradeSampleData.ts
@@ -20,3 +20,29 @@ export const TRADE_SAMPLE_COLLECTIONS: TradeCollectionItem[] = [
 ];
 
 export type TradeFeedItem = {
+  primaryText: string;
+  secondaryText: string;
+  timestamp: string;
+  rightText: string;
+};
+
+export const TRADE_SAMPLE_FEED_ITEMS: TradeFeedItem[] = [
+  {
+    primaryText: "Some walley",
+    secondaryText: "777$",
+    timestamp: "1",
+    rightText: "10,123$",
+  },
+  {
+    primaryText: "Sty. ker",
+    secondaryText: "537$",
+    timestamp: "2",
+    rightText: "9,9999$",
+  },
+  {
+    primaryText: "4iza",
+    secondaryText: "157$",
+    timestamp: "3",
+    rightText: "7111$",
+  },
+];
